3e4a0eca62b018858fbac884a12a9a05a0fb14ac
Sam Halliday <sam.halliday@gmail.com>
2017-07-25 22:24:23 +0100

    shared db

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # job control to use fg to get back to the java process
 set -m
 
-/usr/bin/java -Djava.library.path=/opt/DynamoDBLocal_lib -jar /opt/DynamoDBLocal.jar -dbPath /var/dynamodb_local -port 8000 &
+/usr/bin/java -Djava.library.path=/opt/DynamoDBLocal_lib -jar /opt/DynamoDBLocal.jar -dbPath /var/dynamodb_local -port 8000 -sharedDb &
 
 for A in `find /schemas -type f` ; do
     echo "running schema script $A"
