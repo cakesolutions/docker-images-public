54e17a2549c753fa93beb71483f28a641294fa3e
Sam Halliday <sam.halliday@gmail.com>
2017-09-27 21:14:57 +0100

    shakespeare and concurrency

--- hmonad/run.sh
+++ hmonad/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-stack ghc --system-ghc -- --make /opt/src/hmonad.hs -i -ilib -fforce-recomp  -main-is main -v0 -o /opt/hmonad -odir /tmp/hmonad -hidir /tmp/hmonad
-
-cd /opt/src
+cd /opt/src &&\
+stack ghc --system-ghc -- --make /opt/src/hmonad.hs -i -ilib -fforce-recomp -threaded -main-is main -v0 -o /opt/hmonad -odir /tmp/hmonad -hidir /tmp/hmonad &&\
 exec /opt/hmonad
