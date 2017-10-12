c98abc8605e860ff992ea5aa0896d90cdb9ba8c6
Sam Halliday <sam.halliday@gmail.com>
2017-10-12 14:47:22 +0100

    force empty stack file

--- hmonad/run.sh
+++ hmonad/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /opt/src &&\
-stack ghc --system-ghc -- --make /opt/src/hmonad.hs -i -ilib -fforce-recomp -threaded -main-is main -v0 -O3 -o /opt/hmonad -odir /tmp/hmonad -hidir /tmp/hmonad &&\
+stack ghc --stack-yaml=/root/.stack/global-project/stack.yaml -- --make /opt/src/hmonad.hs -i -ilib -fforce-recomp -threaded -main-is main -v0 -O3 -o /opt/hmonad -odir /tmp/hmonad -hidir /tmp/hmonad &&\
 exec /opt/hmonad
