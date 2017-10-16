09bfc22ab69ff0e3959f46ceda2a9f493b61cf98
martinzapletal <zapletal-martin@email.cz>
2017-10-16 00:12:49 -0400

    Increased kinesis shard limit to avoid warnings/errors when more than the default 10 shards across all streams are created.

--- kinesis/run.sh
+++ kinesis/run.sh
@@ -3,7 +3,7 @@
 # job control to use fg to get back to the main process
 set -m
 
-/usr/bin/kinesalite --ssl &
+/usr/bin/kinesalite --ssl --shardLimit 100 &
 
 for A in `find /schemas -type f` ; do
     echo "running schema script $A"
